24108f7264928411765a793e91faa3d09675f031
Keshav Bansal <keshavbansal0099@gmail.com>
2026-03-29 18:56:02 +0530

    feature: implement Category Controller

--- src/main/java/com/keshavbansal/ECommerceStore/controller/CategoryController.java
+++ src/main/java/com/keshavbansal/ECommerceStore/controller/CategoryController.java
@@ -1,11 +1,87 @@
 package com.keshavbansal.ECommerceStore.controller;
 
+import com.keshavbansal.ECommerceStore.globalException.AlreadyExistsException;
+import com.keshavbansal.ECommerceStore.globalException.ResourceNotFoundException;
+import com.keshavbansal.ECommerceStore.model.Category;
+import com.keshavbansal.ECommerceStore.response.ApiResponse;
+import com.keshavbansal.ECommerceStore.service.category.ICategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
 
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
 public class CategoryController {
+
+    private final ICategoryService categoryService;
+
+    @GetMapping("/get-all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Categories retrieved successfully", categories));
+        } catch (Exception e) {
+            return  ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Failed to retrieve categories", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/add-category")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category category) {
+        try{
+            Category savedCategory = categoryService.addCategory(category);
+            return ResponseEntity.ok(new ApiResponse("Category added successfully",savedCategory));
+
+        }catch (AlreadyExistsException e){
+
+            return  ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+
+        }
+    }
+
+    @GetMapping("/get-by-id/{id}")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id) {
+        try {
+            Category category = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Category retrieved successfully", category));
+        } catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/get-by-name/{name}")
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
+        try {
+            Category category = categoryService.getCategoryByName(name);
+            return ResponseEntity.ok(new ApiResponse("Category retrieved successfully", category));
+        } catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
+    @DeleteMapping("/delete-by-id/{id}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) {
+        try {
+            categoryService.deleteCategory(id);
+            return ResponseEntity.ok(new ApiResponse("Category deleted successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/update-by-id/{id}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        try {
+            Category updatedCategory = categoryService.updateCategory(category,id);
+            return ResponseEntity.ok(new ApiResponse("Category deleted successfully", updatedCategory));
+        } catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
